3202580dd001966e3410390da32c83188d39686c
Freddy_Manuel_Mamani_Cordova <cordovamanuel548@gmail.com>
2025-12-06 04:25:05 -0400

    fea:added componets portfolio

--- utils/motion-transitions.tsx
+++ utils/motion-transitions.tsx
@@ -32,30 +32,20 @@ export const motionTransitionsAbout = {
 
 
 export const fadeIn = (position: string) => {
-    return {
-        visible: {
-            y: 0,
-            x: 0,
-            opacity: 1,
-            transition: {
-                type: "tween",
-                duration: 1.4,
-                delay: 0.5,
-                ease: [0.25, 0.25, 0.25, 0.75],
-            },
-        },
-        hidden: {
-            y: position === 'bottom' ? -80 : 0,
-            x: position === 'right' ? 80 : 0,
-            opacity: 0,
-            transition: {
-                type: "tween",
-                duration: 0.5,
-                delay: 0.5,
-                ease: [0.25, 0.25, 0.25, 0.25],
-            },
-        },
-
-    };
+  return {
+    initial: {
+      y: position === 'bottom' ? -80 : 0,
+      x: position === 'right' ? 80 : 0,
+      opacity: 0,
+      transition: { type: "tween", duration: 0.5, delay: 0.5, ease: [0.25, 0.25, 0.25, 0.25] },
+    },
+    animate: {
+      y: 0,
+      x: 0,
+      opacity: 1,
+      transition: { type: "tween", duration: 1.4, delay: 0.5, ease: [0.25, 0.25, 0.25, 0.75] },
+    },
+  };
 };
+
 
